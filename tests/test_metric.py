7dc16764cbc85bbd767618b47ed9a7a02de1a9b3
julesy <jules89@arcor.de>
2015-04-04 18:02:46 +0200

    Changed test_metric method.

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -7,11 +7,8 @@ class Test(unittest.TestCase):
 
 
     def test_info_gain(self):
-        self.assertEqual(round(metric.info_gain([9,5]),2), 0.940)
+        self.assertEqual(round(metric.calc_gain([9,5]),2), 0.940)
         
-        
-
-
 
 
 if __name__ == "__main__":
